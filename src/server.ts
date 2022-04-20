582bc1ae1e863495773a23f00d42bad6db770257
Elier H <elierporto@gmail.com>
2022-04-20 18:27:13 -0400

    Add: get & post routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import config from "../config";
-
+import todoRoute from "./routes/todos";
 const port = config.port as number;
 const host = config.host as string;
 
 const app: Application = express();
 
-// Body Parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,6 +14,9 @@ app.get("/", (req: Request, res: Response) => {
     message: "Hello World!",
   });
 });
+
+// Routes
+app.use("/todos", todoRoute);
 
 app.listen(port, host, () => {
   console.log(`Server listing at http://${host}:${port}`);
